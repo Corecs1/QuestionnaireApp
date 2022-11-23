17efda3428a3ad532a8fc52d7ef269050ec74fab
Corecs <100100.98@mail.ru>
2022-11-23 23:11:51 +0400

    add a security configurations and add to ignore http request h2 db

--- src/main/java/com/questionnaires/questionnaireapp/controller/securityController/PersonController.java
+++ src/main/java/com/questionnaires/questionnaireapp/controller/securityController/PersonController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-public class UserController {
+public class PersonController {
 
     private final PersonService personService;
 
--- src/main/java/com/questionnaires/questionnaireapp/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/questionnaires/questionnaireapp/security/configuration/SecurityConfiguration.java
@@ -1,4 +1,30 @@
 package com.questionnaires.questionnaireapp.security.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest()
+                .authenticated()).httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**");
+    }
+
 }
